cf2531be5bc1d4a0aa5906f5dbee98f500501e92
IgorZobnev <igor.zobnev@mail.ru>
2021-01-13 11:28:18 +0300

    NavBar is added

--- Ecommerce-app/src/app/components/navbar/navbar.component.ts
+++ Ecommerce-app/src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isOpen: boolean = false;
+
   constructor() { }
+
+  toggleNavBar() {
+    this.isOpen = !this.isOpen;
+  }
 
   ngOnInit(): void {
   }
