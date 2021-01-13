1ad9921cef0ee32993adfcdb2e8932d53d84ec47
IgorZobnev <igor.zobnev@mail.ru>
2021-01-13 10:11:51 +0300

    Add new components

--- Ecommerce-app/src/app/app.module.ts
+++ Ecommerce-app/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AccountComponent } from './components/account/account.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    SingUpComponent,
+    LogoutComponent,
+    CartComponent,
+    AccountComponent,
+    OrdersComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
